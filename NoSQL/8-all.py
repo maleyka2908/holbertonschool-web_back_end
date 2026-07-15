251a7535d6cc306ffc5b574483d16370ac77a7ad
maleyka2908 <13584@holbertonstudents.com>
2026-07-15 19:33:06 +0000

    Add task 8 - list_all documents in Python

---
+++ NoSQL/8-all.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+8-all module
+"""
+
+
+def list_all(mongo_collection):
+    """
+    Lists all documents in a collection.
+    """
+    if mongo_collection is None:
+        return []
+    return list(mongo_collection.find())
