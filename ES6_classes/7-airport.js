2d86d4ad369fd525aa768fc902802ac00db97bb6
maleyka2908 <13584@holbertonstudents.com>
2026-07-21 06:42:20 +0000

    Implement Airport class with default string description override

---
+++ ES6_classes/7-airport.js
@@ -0,0 +1,35 @@
+export default class Airport {
+  constructor(name, code) {
+    this.name = name;
+    this.code = code;
+  }
+
+  // Getter və Setter: name
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    if (typeof newName !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = newName;
+  }
+
+  // Getter və Setter: code
+  get code() {
+    return this._code;
+  }
+
+  set code(newCode) {
+    if (typeof newCode !== 'string') {
+      throw new TypeError('Code must be a string');
+    }
+    this._code = newCode;
+  }
+
+  // Default string description override ([object CODE])
+  get [Symbol.toStringTag]() {
+    return this._code;
+  }
+}
